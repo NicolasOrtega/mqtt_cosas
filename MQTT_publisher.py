68d613cad5f656ce1d394b8ba91fd764db04d6e4
NicolasOrtega <42541336+NicolasOrtega@users.noreply.github.com>
2020-09-22 20:01:40 -0300

    Update MQTT_publisher.py

--- MQTT_publisher.py
+++ MQTT_publisher.py
@@ -2,8 +2,9 @@
 import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
-client.connect('192.168.0.48', 1883)
+client.connect('192.168.0.48', 1883) # Aqui te conectas a la IP del broker, sea cual sea, uno público o la misma jetson.
 
+# Esta secuencia es de prueba para enviar mensajes ingresados por la consola a los tópicos que aparecen en el primer argumento.
 while True:
     client.publish("camara1", input('Message : '))
     client.publish("camara2", input('Message : '))
