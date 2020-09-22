047695ba7548942b94b2fbc64b3681bd334a2124
NicolasOrtega <42541336+NicolasOrtega@users.noreply.github.com>
2020-09-22 20:06:33 -0300

    Update MQTT_suscriptor.py

--- MQTT_suscriptor.py
+++ MQTT_suscriptor.py
@@ -1,18 +1,22 @@
 import paho.mqtt.client as mqtt
 import time
 
+# Esta funcion es la que definen los locos en internet para tener respuesta de lo que está haciendo el cliente MQTT
 def on_log(client, userdata, level, buf):
     print('log: '+buf)
 
+# Con esta función se verifica que el cliente conecte.
 def on_connect(client,userdata,flags,rc):
     if rc == 0:
         print('Conectado!')
     else:
         print('No conectado :(, codigo: ',rc)
 
+# Esta función se ejecuta si el cliente se desconecta
 def on_disconnect(client, userdata, flags, rc):
     print('Desconectado, codigo: '+str(rc))
 
+# Esta función se ejecuta cada vez que llega un mensaje. En este caso la hago imprimir el tópico del mensaje recibido.
 def on_message(client,userdata,msg):
         topic = msg.topic
         print(topic)
@@ -24,6 +28,8 @@ client.on_message = on_message
 
 print('Conectando al broker',broker)
 
+# La secuencia que sigue permite que el cliente esté conectado siempre. Hay otras formas también, en las que el cliente se conecta y reconecta dados ciertos intervalos de tiempo.
+# Esas cosas salen en la documentación de la libreria paho.mqtt
 client.connect(broker)
 client.subscribe([('camara1',0),('camara2',0),('camara3',0)])
 client.loop_forever()
